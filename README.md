6f4765d57ae45805f8178d641f451d295bffc271
William Boman <william@redwill.se>
2018-01-13 19:33:08 +0100

    init

---
+++ README.md
@@ -0,0 +1,76 @@
+*React component for creating highly customizable typewriting visualizations*
+
+[![Build Status](https://travis-ci.org/williamboman/react-typewriting.svg?branch=gh-pages)](https://travis-ci.org/williamboman/react-typewriting/branches)
+
+## Usage
+
+```jsx
+import Typewriting from 'react-typewriting'
+
+<Typewriting
+    strings={[
+        'Sign up today!',
+        'Receive 20% off on your first purchase!',
+    ]}
+/>
+```
+
+```jsx
+import Typewriting from 'react-typewriting'
+
+function Headline({text}) {
+    return <h1>{text}</h1>
+}
+
+<Typewriting
+    stringPropName='text'
+    component={Headline}
+    strings={[
+        'Lorem ipsum',
+            'dolor sit amet',
+    ]}
+/>
+```
+
+## Installation
+
+```sh
+$ npm install react-typewriting
+# or
+$ yarn add react-typewriting
+```
+
+## Props
+
+### `strings` | `Array<string>` | *required*
+
+The strings to print out, in order of appearance.
+
+### `waitBeforeDeleteMs` | `number` | default: 9000
+
+Amount of milliseconds strings will be fully readable before starting
+to delete the string.
+
+### `writeSpeedMs` | `number` | default: 100
+
+The absolute slowest speed to wait between printing characters (characters are printed at random intervals that span from 0 ms to whatever this config value is set to).
+
+### `deleteSpeedMs` | `number` | default: 60
+
+Same as `writeSpeedMs` (see above), but for when deleting characters.
+
+### `component` | `Component | Function | string` | default: 'span'
+
+A `Component`, stateless function, or string corresponding to a default JSX element.
+
+### `stringPropName` | `string` | default: 'children'
+
+The prop to pass the current string value to.
+
+## License
+
+Licensed under the MIT license.
+
+## Authors
+
+**William Boman** <william@redwill.se>
---
+++ src/Typewriting.js
@@ -0,0 +1,168 @@
+const React = require('react')
+const {PureComponent} = React
+const PropTypes = require('prop-types')
+
+const TICK_INIT = 'TICK_INIT'
+const TICK_WRITE = 'TICK_WRITE'
+const TICK_DELETE = 'TICK_DELETE'
+const START_DELETE = 'START_DELETE'
+
+function getComponentProps({...props}) {
+    delete props.strings
+    delete props.stringPropName
+    delete props.waitBeforeDeleteMs
+    delete props.writeSpeedMs
+    delete props.deleteSpeedMs
+    delete props.component
+    return props
+}
+
+class Typewriting extends PureComponent {
+
+    static propTypes = {
+        strings: PropTypes.array.isRequired,
+        stringPropName: PropTypes.string,
+        waitBeforeDeleteMs: PropTypes.number,
+        writeSpeedMs: PropTypes.number,
+        deleteSpeedMs: PropTypes.number,
+        component: PropTypes.oneOfType([
+            PropTypes.func,
+            PropTypes.string,
+        ]),
+    }
+
+    static defaultProps = {
+        waitBeforeDeleteMs: 9000,
+        writeSpeedMs: 100,
+        deleteSpeedMs: 60,
+        stringPropName: 'children',
+        component: 'span',
+    }
+
+    constructor(props) {
+        super(props)
+        this._ticker = null
+        this._ref = null
+        this.state = {
+            currentTextIdx: 0,
+            currentCharPos: 0,
+            isDeleting: false,
+            inputProps: getInputProps(props),
+        }
+    }
+
+    componentDidMount() {
+        this._queueTick(TICK_INIT)
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this._ticker)
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            componentProps: getComponentProps(nextProps),
+        })
+    }
+
+    _randomizeTimeout(ms) {
+        // TODO: probably should implement a minimum timeout
+        return Math.random() * ms
+    }
+
+    _queueTick(type) {
+        const {
+            writeSpeedMs,
+            deleteSpeedMs,
+            waitBeforeDeleteMs,
+        } = this.props
+
+        const timeout =
+            type === TICK_INIT ? 0 :
+            type === TICK_WRITE ? this._randomizeTimeout(writeSpeedMs) :
+            type === TICK_DELETE ? this._randomizeTimeout(deleteSpeedMs) :
+            type === START_DELETE ? waitBeforeDeleteMs :
+            0 // ¯\_(ツ)_/¯
+
+        this._ticker = setTimeout(this._tick, timeout)
+    }
+
+    _moveToNextText() {
+        const {strings} = this.props
+        const {currentTextIdx} = this.state
+        const nextTextIdx = currentTextIdx + 1
+        this.setState({
+            isDeleting: false,
+            currentTextIdx: nextTextIdx < strings.length ? nextTextIdx : 0,
+            currentCharPos: 0,
+        })
+    }
+
+    _tick = () => {
+        const {strings} = this.props
+        const {
+            currentTextIdx,
+            currentCharPos,
+            isDeleting,
+        } = this.state
+
+        const currentText = strings[currentTextIdx]
+
+        const nextCharPos = isDeleting
+            ? currentCharPos - 1
+            : currentCharPos + 1
+
+        if (isDeleting) {
+            if (nextCharPos < 0) {
+                this._moveToNextText()
+            } else {
+                this.setState({
+                    currentCharPos: nextCharPos,
+                })
+            }
+            this._queueTick(TICK_DELETE)
+        } else {
+            if (nextCharPos > currentText.length) {
+                this.setState({
+                    isDeleting: true,
+                })
+                this._queueTick(START_DELETE)
+            } else {
+                this.setState({
+                    currentCharPos: nextCharPos,
+                })
+                this._queueTick(TICK_WRITE)
+            }
+        }
+    }
+
+    render() {
+        const {strings, component} = this.props
+        const {
+            currentTextIdx,
+            currentCharPos,
+            inputProps,
+        } = this.state
+
+        const currentText = strings[currentTextIdx]
+        const text = currentText.slice(0, currentCharPos)
+
+        const componentProps = {
+            ...inputProps,
+            [this.props.stringPropName]: text,
+        }
+
+        if (typeof component === 'string') {
+            return (
+                <component ref={this._registerRef} {...componentProps} />
+            )
+        } else {
+            const Component = component
+            return (
+                <Component ref={this._registerRef} {...componentProps} />
+            )
+        }
+    }
+}
+
+module.exports = Typewriting
